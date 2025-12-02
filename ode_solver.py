8ec184d193c0db03013270f955372f7d0222ebdc
MirteDraaijer <mirtedraaijer@outlook.com>
2025-12-02 09:27:58 +0100

    runge kutta gebruikt als solver, fit methode doet nu ook volume

--- ode_solver.py
+++ ode_solver.py
@@ -35,30 +35,32 @@ class ode_solver:
 
     #     return dagen, volumes
 
-    def solver(self, model):
-        t = 0
-        volume = self.volume
-        dagen = [t]
-        volumes = [volume]
+    # Volgens mij is dit Euler solver:
+    # def solver(self, model):
+    #     t = 0
+    #     volume = self.volume
+    #     dagen = [t]
+    #     volumes = [volume]
 
-        for i in range(self.n):
-            t += self.delta_t
-            delta_volume = model(volume)
-            volume += delta_volume
+    #     for i in range(self.n):
+    #         t += self.delta_t
+    #         delta_volume = model(volume)
+    #         volume += delta_volume
 
-            dagen.append(t)
-            volumes.append(volume)
-        return dagen, volumes
+    #         dagen.append(t)
+    #         volumes.append(volume)
+    #     return dagen, volumes
+    # Runge-Kutta is beter, die proberen te gebruiken
 
-    def lineair(self, c):
-        def model(volume):
-            return volume + (c * self.delta_t)
-        return self.solver(model)
+    def lineair(self, c, volume):
+        def model(y):
+            return y + c
+        return self.runge_kutta_solver(model, volume)
 
-    def exponentieel_toenemend(self, c):
-        def model(volume):
-            return c * volume * self.delta_t
-        return self.solver(model)
+    def exponentieel_toenemend(self, c, volume):
+        def model(y):
+            return c * y
+        return self.runge_kutta_solver(model, volume)
 
     # @DeprecationWarning
     # def mendelsohn_testmodel(self,c):
@@ -79,84 +81,108 @@ class ode_solver:
 
     #     return dagen,volumes
 
-    def mendelsohn(self, c, d):
-        def model(volume):
-            return c * math.pow(volume, d) * self.delta_t
-        return self.solver(model)
+    def mendelsohn(self, c, d, volume):
+        def model(y):
+            return c * math.pow(y, d)
+        return self.runge_kutta_solver(model, volume)
 
-    def exponentieel_afvlakkend(self, c, max_volume):
-        def model(volume):
-            return c * (max_volume - volume) * self.delta_t
-        return self.solver(model)
+    def exponentieel_afvlakkend(self, c, max_volume, volume):
+        def model(y):
+            return c * (max_volume - y)
+        return self.runge_kutta_solver(model, volume)
         
-    def logistisch(self, c,max_volume):
-        def model(volume):
-            return c * volume * (max_volume - volume) * self.delta_t
+    def logistisch(self, c, max_volume, volume):
+        def model(y):
+            return c * y * (max_volume - y)
 
-        return self.solver(model)
+        return self.runge_kutta_solver(model, volume)
 
-    def montroll(self, c, d, max_volume):
-        def model(volume):
-            safe_volume = max(volume, 1e-6)
+    def montroll(self, c, d, max_volume, volume):
+        def model(y):
+            safe_volume = max(y, 1e-6)
             safe_max = max(max_volume, safe_volume)
-            return c * safe_volume * (math.pow(safe_max, d) - math.pow(safe_volume, d)) * self.delta_t
-        return self.solver(model)
+            return c * safe_volume * (math.pow(safe_max, d) - math.pow(safe_volume, d))
+        return self.runge_kutta_solver(model, volume)
 
-    def allee(self, c, min_volume, max_volume):
-        def model(volume):
-            return c * (volume - min_volume) * (max_volume - volume) * self.delta_t
-        return self.solver(model)
+    def allee(self, c, min_volume, max_volume, volume):
+        def model(y):
+            return c * (y - min_volume) * (max_volume - y)
+        return self.runge_kutta_solver(model, volume)
     
-    def lineair_gelimiteerd(self, c, d):
-        def model(volume):
-            return c * (volume / (volume + d)) * self.delta_t
-        return self.solver(model)
+    def lineair_gelimiteerd(self, c, d, volume):
+        def model(y):
+            return c * (y / (y + d))
+        return self.runge_kutta_solver(model, volume)
 
-    def oppervlakte_gelimiteerd(self, c, d):
-        def model(volume):
-            return c * ((volume + d) / 3) * self.delta_t
-        return self.solver(model)
+    def oppervlakte_gelimiteerd(self, c, d, volume):
+        def model(y):
+            return c * ((y + d) / 3)
+        return self.runge_kutta_solver(model, volume)
 
-    def von_bertalanffy(self, c, d):
-        def model(volume):
-            safe_volume = max(volume, 0)
-            return (c * math.pow(safe_volume, 2/3) - d * volume) * self.delta_t
-        return self.solver(model)
+    def von_bertalanffy(self, c, d, volume):
+        def model(y):
+            safe_volume = max(y, 0)
+            return (c * math.pow(safe_volume, 2/3) - d * y)
+        return self.runge_kutta_solver(model, volume)
 
-    def gompertz(self, c, volume_max):
-        def model(volume):
-            if volume <= 0:
-                volume = 1e-6
-            log_calc = math.log((volume_max / volume))
-            result = c * volume * log_calc * self.delta_t
+    def gompertz(self, c, volume_max, volume):
+        def model(y):
+            if y <= 0:
+                y = 1e-6
+            log_calc = math.log((volume_max / y))
+            result = c * y * log_calc
             return result
-        return self.solver(model)
-    
-    def runge_kutta(self, a, b):
-        def ODE(t, y):
-            return a * y + b
+        return self.runge_kutta_solver(model, volume)
+
+    def runge_kutta_solver(self, model, volume):
         t = 0.0
-        y = self.volume
+        y = volume
         dagen = [t]
         volumes = [y]
 
-        for i in range (self.n):
-            dydt1 = ODE(t, y)
+        for i in range (int(self.n)):
+            dydt1 = model(y)
             y1 = y + 0.5 * dydt1 * self.delta_t
 
-            dydt2 = ODE(t, y1)
+            dydt2 = model(y1)
             y2 = y + 0.5 * dydt2 * self.delta_t
 
-            dydt3 = ODE(t, y2)
+            dydt3 = model(y2)
             y3 = y + dydt2 * self.delta_t
 
-            dydt4 = ODE(t, y3)
+            dydt4 = model(y3)
             t = t + self.delta_t
             y = y + (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0 * self.delta_t
 
             dagen.append(t)
             volumes.append(y)
         return dagen, volumes
+
+    # def runge_kutta(self, a, b):
+    #     def ODE(t, y):
+    #         return a * y + b
+    #     t = 0.0
+    #     y = self.volume
+    #     dagen = [t]
+    #     volumes = [y]
+
+    #     for i in range (self.n):
+    #         dydt1 = ODE(t, y)
+    #         y1 = y + 0.5 * dydt1 * self.delta_t
+
+    #         dydt2 = ODE(t, y1)
+    #         y2 = y + 0.5 * dydt2 * self.delta_t
+
+    #         dydt3 = ODE(t, y2)
+    #         y3 = y + dydt2 * self.delta_t
+
+    #         dydt4 = ODE(t, y3)
+    #         t = t + self.delta_t
+    #         y = y + (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0 * self.delta_t
+
+    #         dagen.append(t)
+    #         volumes.append(y)
+    #     return dagen, volumes
     
     def fit(self, echte_volumes, model, params0):
         params = params0.copy()
